6bb8ea2c0748e3e2a1a6d237b806f62b159b0546
Kylecoler <g.kyleallred@yahoo.com>
2019-10-11 11:01:24 -0600

    intial deploy

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const App = express()
 const mongoose = require('mongoose')
 const expressJwt = require('express-jwt')
 require('dotenv').config()
+const path = require("path")
 
 const port = process.env.port || 3003
 
@@ -17,9 +18,13 @@ App.use(express.json())
 App.use('/auth', require('./routes/user_route'))
 App.use('/api', expressJwt({secret: process.env.SECRET}))
 App.use('/api/calendar', require('./routes/event_routes'))
+App.use(express.static(path.join(__dirname, "client", "build")))
 
-mongoose.connect('mongodb://localhost:27017/calendar', {useNewUrlParser: true})
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/calendar', {useNewUrlParser: true})
     .then(console.log("Connected to MongoDB"))
     .catch(err => console.log(err))
+App.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+})
 
 App. listen(port, ()=>console.log(`listening on port ${port}`)) 
